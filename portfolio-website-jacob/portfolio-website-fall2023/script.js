d3df555194eae56a21c8301182383d146e97fc6e
Mr-Zamboni <157420865+Mr-Zamboni@users.noreply.github.com>
2024-04-12 21:52:11 -0500

    Add files via upload
    Website Portfolio 1.0

--- portfolio-website-jacob/portfolio-website-fall2023/script.js
+++ portfolio-website-jacob/portfolio-website-fall2023/script.js
@@ -1,11 +1,14 @@
 // To see if your script properly connected to your index.html
 // Open your website in your browser, go to developer tools 
 // and open the console. 
-// You should see "Hello from Javascript written in the console."
-console.log("Hello from Javascript"); // DELETE ME 
+// You should see "Hello from Javascript written in the console." 
 
 // ADD YOUR toggleMenu() function here
-
-// CODE HERE
-
+function togglemenu() {
+    // CODE HERE
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
 // END
